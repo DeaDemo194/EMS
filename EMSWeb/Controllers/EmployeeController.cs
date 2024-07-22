4cba51da869b474e5fa3dba8eb53456f24c15a5b
Demirhan Söylemez <104253308+DeaDemo194@users.noreply.github.com>
2024-07-22 16:18:16 +0300

    CRUD operations completed

--- EMSWeb/Controllers/EmployeeController.cs
+++ EMSWeb/Controllers/EmployeeController.cs
@@ -17,5 +17,85 @@ namespace EMSWeb.Controllers
             IEnumerable<Employee> objEmployeeList = _db.Employees;
             return View(objEmployeeList);
         }
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Employee emp)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Employees.Add(emp);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(emp);
+        }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var employeeFromDb = _db.Employees.Find(id);
+
+            if (employeeFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(employeeFromDb);
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Employee emp)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Employees.Update(emp);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(emp);
+        }
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var employeeFromDb = _db.Employees.Find(id);
+
+            if (employeeFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(employeeFromDb);
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var emp = _db.Employees.Find(id);
+            if (emp == null)
+            {
+                return NotFound();
+            }
+            _db.Employees.Remove(emp);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+            
+        }
     }
 }
